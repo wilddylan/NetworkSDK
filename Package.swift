eacc7649027181e62629e1ac26a2cc1ea39956fa
Dylan <3664132@163.com>
2016-11-28 13:42:26 +0800

    Add swift package support

--- Package.swift
+++ Package.swift
@@ -1,5 +1,9 @@
 import PackageDescription
 
 let package = Package(
-  name: "NetworkSDK"
+  name: "NetworkSDK",
+  dependencies: [
+    .Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 4.1.0),
+    .Package(url: "https://github.com/Hearst-DD/ObjectMapper.git", majorVersion: 2.2.1)
+  ]
 )
