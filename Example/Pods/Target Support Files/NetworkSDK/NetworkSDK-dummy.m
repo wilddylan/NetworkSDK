45192874bcd94c6bba9fba6bcd38f6445a1ab74d
Dylan <3664132@163.com>
2016-11-28 12:49:48 +0800

    Remove demo project

--- Example/Pods/Target Support Files/NetworkSDK/NetworkSDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_NetworkSDK : NSObject
-@end
-@implementation PodsDummy_NetworkSDK
-@end
--- Example/Pods/Target Support Files/Pods-Network_ExampleTests/Pods-Network_ExampleTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Network_ExampleTests : NSObject
-@end
-@implementation PodsDummy_Pods_Network_ExampleTests
-@end
